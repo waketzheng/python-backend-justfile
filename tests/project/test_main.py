e09f601e7a0d0b15f5508f259588cfc0ed0d70b8
Waket Zheng <waketzheng@gmail.com>
2026-08-16 12:04:54 +0800

    ci: fix lint issue

--- tests/project/test_main.py
+++ tests/project/test_main.py
@@ -1,7 +1,7 @@
 import pytest
 from asynctor.testing import anyio_backend_fixture, async_client_fixture
 
-from app.main import app
+from main import app
 
 anyio_backend = anyio_backend_fixture()
 client = async_client_fixture(app)
