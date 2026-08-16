84e70da7501b4dacc128eb36301abc72b2c4fb85
Waket Zheng <waketzheng@gmail.com>
2026-08-16 16:12:20 +0800

    Move tests to another repo

--- tests/app-project/tests/conftest.py
+++
@@ -1,8 +0,0 @@
-from asynctor.testing import anyio_backend_fixture, async_client_fixture
-from asynctor.utils import ExtendSyspath
-
-with ExtendSyspath():
-    from app.main import app
-
-anyio_backend = anyio_backend_fixture()
-client = async_client_fixture(app)
